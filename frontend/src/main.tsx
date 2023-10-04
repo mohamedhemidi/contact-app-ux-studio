0fd185aa2f8414c7c8024d5f6cb70602c18c5fbc
Mohamed <m.hamidi571@gmail.com>
2023-10-04 10:11:33 +0100

    Vercel production fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+//import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom';
 import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import {store} from "../store.ts";
+import { store } from "../store.ts";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>
+// );
