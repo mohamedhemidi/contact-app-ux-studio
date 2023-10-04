359a270df1f636571b9b1bca05cad5cfb93db263
Mohamed <m.hamidi571@gmail.com>
2023-10-04 13:50:46 +0100

    CORS updated, SSM securing variables, and timeout

---
+++ backend/resources/environement.sh
@@ -0,0 +1,5 @@
+aws ssm put-parameter --name /UX_STUDIO/AWS_S3_BUCKET --value '' --type String --region eu-west-1 --data-type text --profile local-dev
+
+aws ssm put-parameter --name /UX_STUDIO/AWS_ACCESS_KEY --value '' --type String --region eu-west-1 --data-type text --profile local-dev
+
+aws ssm put-parameter --name /UX_STUDIO/AWS_SECRET_ACCESS_KEY --value '' --type String --region eu-west-1 --data-type text --profile local-dev
--- backend/src/libs/cors-origins.ts
+++ backend/src/libs/cors-origins.ts
@@ -1,1 +1,1 @@
-export const origins = ["http://localhost:5173"];
+export const origins = ["http://localhost:5173", "https://contact-app-ux-studio.vercel.app"];
