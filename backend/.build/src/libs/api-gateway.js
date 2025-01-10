5ffb3b5feec3253f39ea548c952473cdb7936546
mohamedhemidi <m.hamidi571@gmail.com>
2025-01-10 12:05:23 +0100

    bundle size

--- backend/.build/src/libs/api-gateway.js
+++
@@ -1,7 +0,0 @@
-export const formatJSONResponse = (statusCode = 200, response) => {
-    return {
-        statusCode,
-        body: JSON.stringify(response),
-    };
-};
-//# sourceMappingURL=api-gateway.js.map
